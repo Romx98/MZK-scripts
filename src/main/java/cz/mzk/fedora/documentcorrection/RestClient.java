dd5dd271fdd9ca2a56f338e8684ffcfa3705e85c
Romx98 <romanklampar@gmail.com>
2021-06-08 11:01:40 +0200

    add getResource, removeAllVC, printAllVC,...

--- src/main/java/cz/mzk/fedora/documentcorrection/RestClient.java
+++ src/main/java/cz/mzk/fedora/documentcorrection/RestClient.java
@@ -1,4 +1,102 @@
 package cz.mzk.fedora.documentcorrection;
 
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.*;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.Objects;
+
 public class RestClient {
+
+    private final String fedoraHost;
+    private final HttpEntity<String> httpEntity;
+    private final RestTemplate restTemplate;
+    private final DocumentBuilder xmlParser;
+    //private final Transformer xmlTransformer;
+    private final XPathExpression xmlPathExp;
+
+    public RestClient(String fh, String fu, String fp)
+            throws ParserConfigurationException, XPathExpressionException {
+        fedoraHost = fh;
+        restTemplate = new RestTemplate();
+        HttpHeaders httpHeaders = createHttpHeaders(fu, fp);
+        httpEntity = new HttpEntity<>(httpHeaders);
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newDefaultInstance();
+        factory.setNamespaceAware(true);
+        xmlParser = factory.newDocumentBuilder();
+
+        XPathFactory xFactory = XPathFactory.newInstance();
+        XPath xmlPath = xFactory.newXPath();
+        xmlPathExp = xmlPath.compile("//*[local-name() = 'isMemberOfCollection']");
+    }
+
+    public Document removeAllVC(Document doc, String vc) throws XPathExpressionException {
+        String uuidVC = getFullFormatVC(vc);
+        NodeList nodes = (NodeList) xmlPathExp.evaluate(doc, XPathConstants.NODESET);
+        for (int i = 0; i < nodes.getLength(); i++) {
+            Node node = nodes.item(i);
+            if (node.getAttributes().item(0).getTextContent().equals(uuidVC)) {
+                Node parent = node.getParentNode();
+                if (parent != null) {
+                    parent.removeChild(node);
+                }
+            }
+        }
+        return doc;
+    }
+
+
+    public void printAllVC(Document doc) throws XPathExpressionException {
+        if (doc == null) { return; }
+
+        NodeList nodes = (NodeList) xmlPathExp.evaluate(doc, XPathConstants.NODESET);
+        System.out.println(nodes.getLength());
+        for (int i = 0; i < nodes.getLength(); i++) {
+            Node node = nodes.item(i);
+            System.out.println(node.getAttributes().item(0).getTextContent());
+        }
+    }
+
+    public Document getFoxmlByUuid(String uuid) {
+        return getFedoraResource(fedoraHost + "/objects/" + uuid + "/objectXML");
+    }
+
+    // get fxml from fedora and return DOM document
+    private Document getFedoraResource(String url) {
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, httpEntity, String.class);
+        String str = Objects.requireNonNull(response.getBody());
+        try {
+            return xmlParser.parse(new InputSource(new StringReader(str)));
+        } catch (SAXException | IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private HttpHeaders createHttpHeaders(String fu, String fp) {
+        String credentials = fu + ":" + fp;
+        String encodeCredentials = new String(Base64.encodeBase64(credentials.getBytes()));
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Basic " + encodeCredentials);
+        return headers;
+    }
+
+    private String getFullFormatVC(String vc) {
+        return "info:fedora/" + vc;
+    }
 }
--- src/main/java/cz/mzk/fedora/documentcorrection/RunRestClient.java
+++ src/main/java/cz/mzk/fedora/documentcorrection/RunRestClient.java
@@ -1,5 +1,24 @@
 package cz.mzk.fedora.documentcorrection;
 
+import org.w3c.dom.Document;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPathExpressionException;
+
 public class RunRestClient {
+
+    public static void main(String[] args)
+            throws ParserConfigurationException, XPathExpressionException {
+        String fh = System.getenv("FH");
+        String fu = System.getenv("FU");
+        String fp = System.getenv("FP");
+        String uuid = "";
+        String vc = "";
+
+        RestClient restClient = new RestClient(fh, fu, fp);
+
+        Document doc = restClient.getFoxmlByUuid(uuid);
+        restClient.printAllVC(doc);
+    }
 
 }
