544da26ff5f70b00f8911d24f87b825254c7decc
Aleksei Ermak <alexej.ermak96@gmail.com>
2021-12-12 11:51:06 +0100

    Add logging and refactor some parts

--- src/main/java/cz/mzk/scripts/SyncFoxmlFieldsWithSolr.java
+++ src/main/java/cz/mzk/scripts/SyncFoxmlFieldsWithSolr.java
@@ -10,6 +10,7 @@ import cz.mzk.utils.FoxmlUtils;
 import cz.mzk.utils.SolrUtils;
 import lombok.Getter;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.common.SolrDocument;
 import org.apache.solr.common.SolrInputDocument;
@@ -18,6 +19,7 @@ import org.w3c.dom.Document;
 import java.util.*;
 import java.util.function.BiConsumer;
 
+@Slf4j
 public class SyncFoxmlFieldsWithSolr extends Script {
 
     private final ParamMap paramMap;
@@ -25,6 +27,7 @@ public class SyncFoxmlFieldsWithSolr extends Script {
     private static final FoxmlUtils foxmlUtils = new FoxmlUtils();
     private static final FileWrapper updateIssnCnbRootsFileWriter = new FileWrapper("updated-issn-cnb-roots");
     private static final FileWrapper missingDocInFedora = new FileWrapper("missing-doc-in-fedora");
+    private static final FileWrapper skipped = new FileWrapper("skipping-due-to-exception");
 
     public static void main(String[] args) {
         final Map<String, Object> params = new HashMap<>() {{
@@ -46,61 +49,75 @@ public class SyncFoxmlFieldsWithSolr extends Script {
     @Override
     public void run() {
         final CustomSolrClient solrClient = new CustomSolrClient(paramMap.getSolrHost());
-        final FedoraRestClient fedoraClient = new FedoraRestClient(paramMap.getFedoraHost(), paramMap.getFedoraUser(), paramMap.getFedoraPswd());
+        final FedoraRestClient fedoraClient = new FedoraRestClient(
+                paramMap.getFedoraHost(), paramMap.getFedoraUser(), paramMap.getFedoraPswd()
+        );
 
         final List<BiConsumer<Document, SolrInputDocument>> fieldSynchronizers = new ArrayList<>() {{
             add(createCNBSynchronizer());
             add(createISSNSynchronizer());
         }};
 
-        final SolrQuery fetchAllRootsSolrQuery = createFetchAllRootsWithoutTheRequiredFieldsSolrQuery();
+        final SolrQuery fetchAllRootsSolrQuery = createFetchAllRootsWithoutRequiredFieldsSolrQuery();
 
         solrClient.queryAndApply(fetchAllRootsSolrQuery, (solrDoc) -> {
-            final String uuidDoc = (String) solrDoc.getFieldValue(SolrField.UUID);
-            final SolrInputDocument solrInputDoc = createSolrInputDocument(solrDoc);
-            final Optional<Document> foxml = fedoraClient.getDC(uuidDoc);
+            final String uuid = (String) solrDoc.getFieldValue(SolrField.UUID);
+            try {
+                final SolrInputDocument inputDoc = createSolrInputDoc(solrDoc);
+                final Optional<Document> dcDatastream = fedoraClient.getDC(uuid);
 
-            foxml.ifPresentOrElse(doc -> fieldSynchronizers.forEach(
-                            x -> x.accept(doc, solrInputDoc)),
-                    () -> missingDocInFedora.writeLine(uuidDoc)
-            );
+                log.info("Process " + uuid + "...");
 
-            if (solrInputDoc.containsKey(SolrField.MODIFIED_DATE)) {
-                solrClient.addSolrInputDocument(solrInputDoc);
-                updateIssnCnbRootsFileWriter.writeLine(uuidDoc);
+                dcDatastream.ifPresentOrElse(dc ->
+                        fieldSynchronizers.forEach(x -> x.accept(dc, inputDoc)),
+                        () -> {
+                            log.warn("No DC datastream for " + uuid + "!");
+                            missingDocInFedora.writeLine(uuid);
+                        }
+                );
+
+                if (isModified(inputDoc) && !paramMap.isDryMode()) {
+                    log.info("Update " + uuid + " in destination Solr instance.");
+                    solrClient.addSolrInputDocument(inputDoc);
+                    updateIssnCnbRootsFileWriter.writeLine(uuid);
+                }
+            } catch (final Exception e) {
+                log.warn("An exception occurred: " + e.getMessage());
+                skipped.writeLine(uuid);
             }
-
         }, MAX_DOCS_PER_SOLR_QUERY);
 
+        solrClient.close(true);
         updateIssnCnbRootsFileWriter.closeFile();
         missingDocInFedora.closeFile();
-        solrClient.close(true);
     }
 
     private static BiConsumer<Document, SolrInputDocument> createCNBSynchronizer() {
-        return (foxml, solrInputDoc) -> {
-            final List<String> listOfCNB = foxmlUtils.getListOfCNBFromFoxml(foxml);
+        return (dcDatastream, solrInputDoc) -> {
+            final List<String> listOfCNB = foxmlUtils.getListOfCNBFromFoxml(dcDatastream);
             if (!listOfCNB.isEmpty()) {
-                final Collection<Object> solrIdentifier = solrInputDoc.getFieldValues(SolrField.DC_IDENT);
-                solrIdentifier.addAll(listOfCNB);
+                log.info("Found new CNB flags: " + listOfCNB);
+                final Collection<Object> dcIdentifiers = solrInputDoc.getFieldValues(SolrField.DC_IDENT);
+                dcIdentifiers.addAll(listOfCNB);
 
-                SolrUtils.setModify(solrInputDoc, SolrField.DC_IDENT, solrIdentifier);
+                SolrUtils.setModify(solrInputDoc, SolrField.DC_IDENT, dcIdentifiers);
                 SolrUtils.setModify(solrInputDoc, SolrField.MODIFIED_DATE, SolrUtils.getCurrentTimeDate());
             }
         };
     }
 
     private static BiConsumer<Document, SolrInputDocument> createISSNSynchronizer() {
-        return (foxml, solrInputDoc) -> {
-            final Optional<String> issn = foxmlUtils.getStrISSNFromFoxml(foxml);
-            if (issn.isPresent()) {
+        return (dcDatastream, solrInputDoc) -> {
+            final Optional<String> optionalIssn = foxmlUtils.getStrISSNFromFoxml(dcDatastream);
+            optionalIssn.ifPresent(issn -> {
+                log.info("Found new ISSN flag: " + issn);
                 SolrUtils.setModify(solrInputDoc, SolrField.ISSN, issn);
                 SolrUtils.setModify(solrInputDoc, SolrField.MODIFIED_DATE, SolrUtils.getCurrentTimeDate());
-            }
+            });
         };
     }
 
-    private static SolrQuery createFetchAllRootsWithoutTheRequiredFieldsSolrQuery() {
+    private static SolrQuery createFetchAllRootsWithoutRequiredFieldsSolrQuery() {
         final SolrQuery solrQuery = new SolrQuery();
         solrQuery.setQuery(
                 (SolrUtils.queryNoFieldValue(SolrField.ISSN) + " OR " +
@@ -116,11 +133,15 @@ public class SyncFoxmlFieldsWithSolr extends Script {
         return solrQuery;
     }
 
-    private static SolrInputDocument createSolrInputDocument(final SolrDocument solrDoc) {
+    private static SolrInputDocument createSolrInputDoc(final SolrDocument solrDoc) {
         final Collection<Object> existingDcIdent = solrDoc.getFieldValues(SolrField.DC_IDENT);
         final SolrInputDocument inputDoc = new SolrInputDocument();
         inputDoc.addField(SolrField.DC_IDENT, existingDcIdent != null ? existingDcIdent : Collections.emptyList());
         return inputDoc;
+    }
+
+    private boolean isModified(final SolrInputDocument inputDoc) {
+        return inputDoc.containsKey(SolrField.MODIFIED_DATE);
     }
 
     @Getter
@@ -129,12 +150,14 @@ public class SyncFoxmlFieldsWithSolr extends Script {
         final String fedoraUser;
         final String fedoraPswd;
         final String solrHost;
+        final boolean dryMode;
 
         public ParamMap(final Map<String, Object> params) {
             fedoraHost = (String) params.getOrDefault(EnvironmentParam.FEDORA_HOST.name(), null);
             fedoraUser = (String) params.getOrDefault(EnvironmentParam.FEDORA_USER.name(), null);
             fedoraPswd = (String) params.getOrDefault(EnvironmentParam.FEDORA_PSWD.name(), null);
             solrHost = (String) params.getOrDefault(EnvironmentParam.SOLR_HOST.name(), null);
+            dryMode = (boolean) params.getOrDefault("DRY_MODE", false);
         }
     }
 }
